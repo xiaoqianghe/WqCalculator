43a88cb3720497f9b227f3ea5958895aedd31a82
小强何 <1565706716@qq.com>
2018-03-27 15:03:54 +0800

    hwq-添加了获取网络图片的页面

--- pages/photo/photo.js
+++ pages/photo/photo.js
@@ -5,13 +5,135 @@ Page({
    * 页面的初始数据
    */
   data: {
+
+
+    meizi: {},
+    page: 1,
+    isLoadMore: false,
   
   },
+
+
+  onImageTap: function (event) {
+    var imageIndex = event.currentTarget.dataset.imageIndex;
+    var imageUrl = this.data.meizi.results[imageIndex].url;
+    wx.navigateTo({
+      url: '/pages/meizi/show-image/show-image?imageUrl=' + imageUrl + '&fromWhere=meizi',
+      success: function (res) {
+        // success
+      },
+      fail: function () {
+        // fail
+      },
+      complete: function () {
+        // complete
+      }
+    })
+  },
+
+ 
+
+
+  onReachBottom: function () {
+    this.loadMoreData();
+  },
+
+  loadMoreData: function (event) {
+    if (this.data.isLoadMore) {
+      return;
+    }
+    this.setData({ isLoadMore: true });
+    var that = this;
+    var page = this.data.page + 1;
+    wx.request({
+      url: 'http://gank.io/api/data/%E7%A6%8F%E5%88%A9/10/' + page,
+
+      // url: 'http://gank.io/api/data/%E7%A6%8F%E5%88%A9/10/1',
+      // method: 'GET',
+      data: {
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        var meizi = res.data;
+        var thisMeizi = that.data.meizi;
+        console.log(meizi);
+        if (!meizi.error) {
+          thisMeizi.results = thisMeizi.results.concat(meizi.results);
+          that.setData({
+            meizi: thisMeizi
+          });
+        }
+        wx.stopPullDownRefresh()
+        that.setData({
+          page: that.data.page + 1
+        });
+      },
+      fail: function () {
+
+      },
+      complete: function () {
+        that.setData({ isLoadMore: false });
+      }
+
+    })
+  },
+
+
+
+
+
+
+  onPullDownRefresh: function () {
+    this.refreshData();
+  },
+
+
+  refreshData: function () {
+    var that = this;
+    wx.request({
+      url: 'http://gank.io/api/data/%E7%A6%8F%E5%88%A9/10/1',
+      data: {
+      },
+      // method: 'GET',
+      header: {
+        'content-type': 'application/json',
+      },
+      success: function (res) {
+        var meizi = res.data;
+        console.log(meizi);
+        that.setData({
+          meizi: meizi.error ? {} : meizi
+        });
+      },
+      fail: function () {
+
+
+
+        console.log("=====fail");
+
+        
+
+      },
+      complete: function () {
+        wx.stopPullDownRefresh()
+      }
+
+    })
+  },
+
+
+
+
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+
+    // 页面初始化 options为页面跳转所带来的参数
+    this.refreshData();
   
   },
 
@@ -43,19 +165,19 @@ Page({
   
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
+  // /**
+  //  * 页面相关事件处理函数--监听用户下拉动作
+  //  */
+  // onPullDownRefresh: function () {
   
-  },
+  // },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
+  // /**
+  //  * 页面上拉触底事件的处理函数
+  //  */
+  // onReachBottom: function () {
   
-  },
+  // },
 
   /**
    * 用户点击右上角分享
